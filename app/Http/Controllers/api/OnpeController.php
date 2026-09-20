bbc99378a7a61a3b1c36ab5744477a0e1021fd28
saenz <saenzgastelulrm@gmail.com>
2026-09-20 18:18:15 -0500

    Actualizacion final

--- app/Http/Controllers/api/OnpeController.php
+++ app/Http/Controllers/api/OnpeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -8,19 +8,106 @@ use Illuminate\Support\Facades\DB;
 
 class OnpeController extends Controller
 {
-    public function grupo_votacion($grupo_votacion)
-    {
-        $acta = DB::select("call sp_getGrupoVotacion(?)", [$grupo_votacion]);
-        
-        $success = isset($acta);
+    private function generarRespuesta($data, $clave, $msjExito, $msjError) {
+        $success = !empty($data);
         $status = $success ? 200 : 404;
 
-        $acta = [
-            'success' => $success,
-            'data' => $success ? $acta : null,
-            'message' => $success ? 'Acta encontrada' : 'No existe esta acta',
-            'status' => $status
-        ];
-        return response()->json($acta, $status);
+        return response()->json([
+            "success" => $success,
+            $clave => $success ? $data : null,
+            "message" => $success ? $msjExito : $msjError,
+            "status" => $status
+        ], $status);
     }
+
+    public function getGrupoVotacion($grupoVotacion) {
+        $acta = DB::select("call sp_getGrupoVotacion(?)", [$grupoVotacion]);
+        return $this->generarRespuesta($acta, 'acta', 'Acta encontrada', 'No existe esta acta');
+    }
+
+    public function isDepartamento($detalle) { 
+        $departamento = DB::select("call sp_isDepartamento(?)", [$detalle]); 
+        return $this->generarRespuesta($departamento, 'departamento', 'Departamento encontrado', 'No existe el departamento');
+    }
+
+    public function isProvincia($detalle) { 
+        $provincia = DB::select("call sp_isProvincia(?)", [$detalle]); 
+        return $this->generarRespuesta($provincia, 'provincia', 'Provincia encontrada', 'No existe la provincia');
+    }
+
+    public function getDepartamentos($inicio, $fin) { 
+        $departamentos = DB::select("call sp_getDepartamentos(?, ?)", [$inicio, $fin]); 
+        return $this->generarRespuesta($departamentos, 'departamentos', 'Departamentos encontrados', 'No existen los departamentos');
+    }
+
+    public function getProvincias($idDepartamento) { 
+        $provincias = DB::select("call sp_getProvincias(?)", [$idDepartamento]); 
+        return $this->generarRespuesta($provincias, 'provincias', 'Provincias encontradas', 'No existen las provincias');
+    }
+
+    public function getProvinciasByDepartamento($departamento) { 
+        $provincias = DB::select("call sp_getProvinciasbyDepartamento(?)", [$departamento]); 
+        return $this->generarRespuesta($provincias, 'provincias', 'Provincias encontradas', 'No existen las provincias');
+    }
+
+    public function getDistritos($idProvincia) { 
+        $distritos = DB::select("call sp_getDistritos(?)", [$idProvincia]); 
+        return $this->generarRespuesta($distritos, 'distritos', 'Distritos encontrados', 'No existen los distritos');
+    }
+
+    public function getDistritosByProvincia($provincia) { 
+        $distritos = DB::select("call sp_getDistritosByProvincia(?)", [$provincia]); 
+        return $this->generarRespuesta($distritos, 'distritos', 'Distritos encontrados', 'No existen los distritos');
+    }
+
+    public function getLocalesVotacion($idDistrito) { 
+        $locales = DB::select("call sp_getLocalesVotacion(?)", [$idDistrito]); 
+        return $this->generarRespuesta($locales, 'locales', 'Locales encontrados', 'No existen los locales');
+    }
+
+    public function getLocalesVotacionByDistrito($provincia, $distrito) { 
+        $locales = DB::select("call sp_getLocalesVotacionByDistrito(?, ?)", [$provincia, $distrito]); 
+        return $this->generarRespuesta($locales, 'locales', 'Locales encontrados', 'No existen los locales');
+    }
+
+    public function getGruposVotacion($idLocalVotacion) { 
+        $grupos = DB::select("call sp_getGruposVotacion(?)", [$idLocalVotacion]); 
+        return $this->generarRespuesta($grupos, 'grupos', 'Grupos encontrados', 'No existen los grupos');
+    }
+
+    public function getGruposVotacionByUbicacion($provincia, $distrito, $local) { 
+        $grupos = DB::select("call sp_getGruposVotacionByProvinciaDistritoLocal(?, ?, ?)", [$provincia, $distrito, $local]); 
+        return $this->generarRespuesta($grupos, 'grupos', 'Grupos encontrados', 'No existen los grupos');
+    }
+
+    public function getGrupoVotacionDetalle($departamento, $provincia, $distrito, $local, $grupo) { 
+        $acta = DB::select("call sp_getGrupoVotacionByProvinciaDistritoLocalGrupo(?, ?, ?, ?, ?)", [$departamento, $provincia, $distrito, $local, $grupo]); 
+        return $this->generarRespuesta($acta, 'acta', 'Acta encontrada', 'No existe esta acta');
+    }
+
+    public function getVotos($inicio, $fin) { 
+        $votos = DB::select("call sp_getVotos(?, ?)", [$inicio, $fin]); 
+        return $this->generarRespuesta($votos, 'votos', 'Votos encontrados', 'No existen votos');
+    }
+
+    public function getVotosByDepartamento($departamento) { 
+        $votos = DB::select("call sp_getVotosDepartamento(?)", [$departamento]); 
+        return $this->generarRespuesta($votos, 'votos', 'Votos encontrados', 'No existen votos');
+    }
+
+    public function getVotosByProvincia($provincia) { 
+        $votos = DB::select("call sp_getVotosProvincia(?)", [$provincia]); 
+        return $this->generarRespuesta($votos, 'votos', 'Votos encontrados', 'No existen votos');
+    }
+
+    public function getDistritosByDepartamento($departamento) { 
+        $distritos = DB::select("call sp_getDistritosDepartamento(?)", [$departamento]); 
+        return $this->generarRespuesta($distritos, 'distritos', 'Distritos encontrados', 'No existen los distritos');
+    }
+
+    public function getLocalesVotacionByDepartamento($departamento) { 
+        $locales = DB::select("call sp_getLocalesVotacionDepartamento(?)", [$departamento]); 
+        return $this->generarRespuesta($locales, 'locales', 'Locales encontrados', 'No existen los locales');
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\OnpeController;
 
 Route::controller(OnpeController::class)->group(function () {
-    Route::get('grupo_votacion/{grupo_votacion}', 'grupo_votacion');
+    Route::get('grupo_votacion/{grupo_votacion}', 'getGrupoVotacion');
+    Route::get('is_departamento/{detalle}', 'isDepartamento'); 
+    Route::get('is_provincia/{detalle}', 'isProvincia');
+    Route::get('departamentos/{inicio}/{fin}', 'getDepartamentos');
+    Route::get('provincias/{idDepartamento}', 'getProvincias');
+    Route::get('provincias_departamento/{departamento}', 'getProvinciasByDepartamento');
+    Route::get('distritos/{idProvincia}', 'getDistritos'); 
+    Route::get('distritos_provincia/{provincia}', 'getDistritosByProvincia');
+    Route::get('locales_votacion/{idDistrito}', 'getLocalesVotacion'); 
+    Route::get('locales_votacion_distrito/{provincia}/{distrito}', 'getLocalesVotacionByDistrito');
+    Route::get('grupos_votacion/{idLocalVotacion}', 'getGruposVotacion'); 
+    Route::get('grupos_votacion_ubicacion/{provincia}/{distrito}/{local}', 'getGruposVotacionByUbicacion');
+    Route::get('grupo_votacion_detalle/{departamento}/{provincia}/{distrito}/{local}/{grupo}', 'getGrupoVotacionDetalle');
+    Route::get('votos/{inicio}/{fin}', 'getVotos'); 
+    Route::get('votos_departamento/{departamento}', 'getVotosByDepartamento'); 
+    Route::get('votos_provincia/{provincia}', 'getVotosByProvincia');
+    Route::get('distritos_departamento/{departamento}', 'getDistritosByDepartamento'); 
+    Route::get('locales_votacion_departamento/{departamento}', 'getLocalesVotacionByDepartamento');
 });
